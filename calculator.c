8390d0f3508acab3ce349ff0498f17908d33705f
Ewan <ewan_hou@gemteks.com>
2014-07-15 11:32:32 +0800

    modify a print msg

--- calculator.c
+++ calculator.c
@@ -66,6 +66,7 @@ int main(int argc, char *argv[])
 			OP_DIVIDER(num_1, num_2, &result);
 			printf("\ndarcy 2014 07 15 AM 11:00 \n");
 			printf("\ndarcy 2014 07 15 AM 11:05 \n");
+			printf("\newan 2014 07 15 AM 11:30 \n");
 			break;	
 		default:
 			printf("Operator [%c] is incorrect\n", operator);
